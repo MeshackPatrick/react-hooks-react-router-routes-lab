40f819062cf83f9f2ae3c1a2c7c956cd415f6240
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-07-26 17:03:39 +0300

    react routes,navlink and switch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+    <NavLink to="/" exact="true">Home</NavLink><br/>
+    <NavLink to="/movies" exact="true">Movies</NavLink><br/>
+    <NavLink to="/directors" exact="true">Directors</NavLink><br/>
+    <NavLink to="/actors" exact="true">Actors</NavLink>
+  </div>;
 }
 
 export default NavBar;
